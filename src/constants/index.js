506bb4e1d7b6c154d32ace8b84335f372c8d21b7
huzai7a-dev <shaikhhuzaifa072@gmail.com>
2024-02-02 00:39:59 +0500

    resume added

---
+++ src/constants/index.js
@@ -0,0 +1,274 @@
+import {
+  frontend,
+  backend,
+  ux,
+  prototyping,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  git,
+  docker,
+  postgresql,
+  graphql,
+  komikult,
+  leaderboard,
+  math,
+  movie,
+  nyeusi,
+  coverhunt,
+  dcc,
+  kelhel,
+  microverse,
+  nest,
+  mongodb,
+} from "../assets";
+
+export const devName = "Muhammad Huzaifa";
+export const devTitle = "A full stack developer with 3 years of experience";
+export const devIntroduction = `As a MERN Stack Developer, I bring a robust skill set in MongoDB, Express.js, React.js, and Node.js, enabling me to craft dynamic, end-to-end web applications with ease. My expertise lies in creating responsive user interfaces, building scalable backend architectures, and managing databases efficiently. With a keen eye for both functionality and aesthetics, I'm dedicated to delivering seamless and performant digital experiences that meet the evolving needs of users and businesses alike. Whether it's developing feature-rich platforms or optimizing existing systems, my goal is to drive innovation through cutting-edge web technologies.`;
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "projects",
+    title: "Projects",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Pixel Perfect UI",
+    icon: ux,
+  },
+  {
+    title: "Frontend Developer",
+    icon: frontend,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "Database Design",
+    icon: prototyping,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "Nest JS",
+    icon: nest,
+  },
+  {
+    name: "Mongo DB",
+    icon: mongodb,
+  },
+  // {
+  //   name: "Rails",
+  //   icon: rubyrails,
+  // },
+  {
+    name: "graphql",
+    icon: graphql,
+  },
+  {
+    name: "postgresql",
+    icon: postgresql,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  // {
+  //   name: "figma",
+  //   icon: figma,
+  // },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: "Front-End Internee",
+    company_name: "Bizz World",
+    icon: coverhunt,
+    iconBg: "#333333",
+    date: "March 2021 - May 2021",
+  },
+  {
+    title: "React Developer",
+    company_name: "Bizz World",
+    icon: microverse,
+    iconBg: "#333333",
+    date: "July 2021 - July 2022",
+  },
+  {
+    title: "MERN Stack Developer",
+    company_name: "Codesy Consulting",
+    icon: kelhel,
+    iconBg: "#333333",
+    date: "March 2022 - Present",
+  },
+];
+
+const projects = [
+  {
+    id: "project-1",
+    name: "KomiKult",
+    description: "A comic characters list app that displays Marvel characters.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: komikult,
+    repo: "https://github.com/shaqdeff/KomiKult",
+    demo: "https://shaqdeff.github.io/KomiKult/",
+  },
+  {
+    id: "project-2",
+    name: "Leaderboard",
+    description:
+      "A leaderboard list app that displays scores submitted by different players.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: leaderboard,
+    repo: "https://github.com/shaqdeff/Leaderboard",
+    demo: "https://shaqdeff.github.io/Leaderboard/",
+  },
+  {
+    id: "project-3",
+    name: "Math Magicians",
+    description: "This is a single-page calculator app built with React",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: math,
+    repo: "https://github.com/shaqdeff/Math-Magicians",
+    demo: "https://inspiring-medovik-37d3b3.netlify.app/",
+  },
+  {
+    id: "project-4",
+    name: "Movie Metro",
+    description: `A single-page application that allows users to search for any movie or show's ratings and its details.`,
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: movie,
+    repo: "https://github.com/shaqdeff/Movie-Metro",
+    demo: "https://movie-metro.netlify.app/",
+  },
+  {
+    id: "project-5",
+    name: "Nyeusi Fest Site",
+    description:
+      "This is a demo concert website for a music festival called Nyeusi.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: nyeusi,
+    repo: "https://github.com/shaqdeff/Nyeusi-Fest-Site",
+    demo: "https://shaqdeff.github.io/Nyeusi-Fest-Site/",
+  },
+];
+
+export { services, technologies, experiences, projects };
